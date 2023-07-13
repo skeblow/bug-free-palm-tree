5adce49a4ee5bc91f7e19283680ff3210d1fcde7
skeblow <skeblow@gmail.com>
2023-07-13 20:48:24 +0200

    Stuff

--- db/queries/Item.ts
+++ db/queries/Item.ts
@@ -17,6 +17,14 @@ export async function selectAllItems (db: Database, filter: ItemFilter): Promise
     conditions.push(`engine IN (${engines})`)
   }
 
+  if (filter.year_from > 0) {
+    conditions.push(`year >= ${filter.year_from}`)
+  }
+
+  if (filter.year_to > 0) {
+    conditions.push(`year <= ${filter.year_to}`)
+  }
+
   let where = '';
 
   if (conditions.length > 0) {
--- service/Bazos.ts
+++ service/Bazos.ts
@@ -56,6 +56,7 @@ export async function fetchAllBazos (): Promise<Array<Item>> {
     }
   })
   .filter((item: Item): boolean => item.title.toLocaleLowerCase().includes('subaru'))
+  .filter((item: Item): boolean => ! item.title.toLocaleLowerCase().includes('koupím'))
 }
 
 export async function fetchOneBazos(url: string): Promise<Item> {
@@ -118,16 +119,84 @@ export function parseBazosItem (db: Database, item: Item): Item {
 }
 
 function parseEngine (text: string): string|null {
-  let matches = text.match(/(forester|motor|outback|benzin) \w?\w? ?([1,2,3][\.\,][0,5,6])/i)
+  text = text.replace(' 4x4', '')
+
+  let matches = text.match(/(forester|motor|outback|benzin|legacy|subaru|tribeca|impreza|wrx|levorg|ombi|justy)e?m? \w?\w? ?([1,2,3][\.\,][0,5,6])/i)
 
   if (matches && matches[2]) {
     return matches[2].replace(',', '.')
   }
 
-  matches = text.match(/2.5T/i)
+  const oneAndSixLiter = [
+    /1.6 (gts|t)/i,
+    /1.6i/i
+  ]
 
-  if (matches) {
-    return '2.5'
+  const twoLiter = [
+    /2.0 ?x?T/i,
+    /(dvoulitr|2,0) benzín/i,
+    /1 995 cm3/i,
+    /2.0 (Boxer|l)/i,
+    /2.od/i,
+    /FB20/i,
+    /DIESEL 2.0D/i
+  ]
+
+  const twoAndHalfLiter = [
+    /2.5 ?T/i,
+    /motor ej25/i,
+    /2.5 ?i/i,
+    /2457cm3/i,
+    /2500ccm/i,
+    /2.5litru/i
+  ]
+
+  const threeLiter = [
+    /3.0h6/i
+  ]
+
+  const threeAndSixLiter = [
+    /3 630 ccm/i
+  ]
+
+  for (const pattern of oneAndSixLiter) {
+    matches = text.match(pattern)
+
+    if (matches) {
+      return '1.6'
+    }
+  }
+
+  for (const pattern of twoLiter) {
+    matches = text.match(pattern)
+
+    if (matches) {
+      return '2.0'
+    }
+  }
+
+  for (const pattern of twoAndHalfLiter) {
+    matches = text.match(pattern)
+
+    if (matches) {
+      return '2.5'
+    }
+  }
+
+  for (const pattern of threeLiter) {
+    matches = text.match(pattern)
+
+    if (matches) {
+      return '3.0'
+    }
+  }
+
+  for (const pattern of threeAndSixLiter) {
+    matches = text.match(pattern)
+
+    if (matches) {
+      return '3.6'
+    }
   }
 
   return null
